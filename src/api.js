acd4a86eafa772e34b033eaa5982050fe1ec1914
kelvinabche <kelvinabache12@gmail.com>
2022-08-31 16:57:00 -0430

    terminado la parte logica

--- src/api.js
+++ src/api.js
@@ -1,13 +1,25 @@
-import {buscador} from "./app.js";
+/*llamando a la contendor*/
+import {caja} from "./app.js";
 
-async function api() {
+export async function api(buscardor) {
+
   /*datos  para buscar*/
-  const DosApi = await fetch(`https://dog.ceo/api/breeds/${buscador}/images/random`);
+  const DosApi = await fetch(`https://dog.ceo/api/breed/${buscardor}/images/random`);
   const datos = DosApi.json();
 
   /*verificar si no tenemos error de busqueda */
-  if (DosApi.status === 404 || DosApi === 405) console.error("tenemos problemas");
-  else  datos.then((mostra) => {console.log(mostra) });
+  if (DosApi.status === 404 || DosApi.status === 405)
+    console.error("tenemos problemas");
+  else
+       datos.then((mostra) => {
+         recorrer(mostra.message);
+    });
 }
 
-api();
+/*estructurando forma visual*/
+function recorrer(perros){
+    caja.innerHTML += `<img src=${perros}> <br><br> <p>${perros}</p>`;
+}
+
+
+
--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+const buscardor = document.querySelector('input');
+export const caja = document.getElementById('caja');
 
-export const buscador = document.getElementById('buscador');
+/*exportando la api*/
+import {api} from "./api.js";
+
+/*comienza el envio*/
+const btn = document.getElementById('btn');
+ btn.addEventListener('click',valor);
+
+ /*validando la busquedad */
+ function valor(e) {
+   e.preventDefault();
+  api(buscardor.value);
+ }
 
